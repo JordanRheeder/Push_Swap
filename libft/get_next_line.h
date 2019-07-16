b7994b1b729f642457c5a1bfc5211110a3e999cb
Jordan Rheeder <Jordanrheeder@gmail.com>
2019-07-16 12:21:22 +0200

    Add files via upload

---
+++ libft/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrheeder <jrheeder@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/30 19:18:24 by jrheeder          #+#    #+#             */
+/*   Updated: 2019/07/12 15:18:39 by jrheeder         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# define BUFF_SIZE 100
+
+int		get_next_line(const int fd, char **line);
+
+#endif
